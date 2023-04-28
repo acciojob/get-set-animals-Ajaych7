a1841038cd61528fdb81d1197ecca9dd2d8f36f0
AJAYCH <91587077+Ajaych7@users.noreply.github.com>
2023-04-28 06:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-class Animal {
+class Animal{
   constructor(species) {
     this._species = species;
   }
-
-  get species() {
+  get species(){
     return this._species;
   }
-
-  makeSound() {
+  makeSound(){
     console.log("The animal makes a sound");
   }
 }
-class Cat extends Animal {
+class Cat extends Animal{
   purr() {
     console.log("purr");
   }
 }
-
-class Dog extends Animal {
+class Dog extends Animal{
   bark() {
     console.log("woof");
   }
